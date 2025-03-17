2d223c8937599c8c3f46154530bcfefd9c561ee3
Huni0128 <tlacogns@gmail.com>
2025-03-17 21:07:34 +0900

    add man_women.png pie plot

--- health.py
+++ health.py
@@ -1,5 +1,17 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
 
-health_data = pd.read_excel("./data/non_health.xlsx")
-health_data.info()
-#print(health_data)
+matplotlib.rc("font", family="NanumGothic")
+
+health_data = pd.read_excel("./data/health_data.xlsx")
+
+health_data = health_data.drop(columns="시점") # 시점 칼럼 삭제
+health_data = health_data[health_data["대분류"]=="성별"]
+health_data = health_data.rename(columns={"합계": "운동을 할 충분한 시간이 없어서"})
+health_data = health_data.set_index("분류")
+
+plt.figure(figsize=(10,8))
+health_data["운동을 할 충분한 시간이 없어서"].plot.pie(explode=(0,0.02))
+plt.savefig("man_women.png")
+print(health_data)
